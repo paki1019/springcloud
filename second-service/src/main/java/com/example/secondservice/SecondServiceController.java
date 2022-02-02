adf5ebb4f95f5a3de42652da6bffc03324254853
paki1019 <paki1019@gmail.com>
2022-02-02 17:59:07 +0900

    springcloud

---
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,38 @@
+package com.example.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+@Slf4j
+public class SecondServiceController {
+    Environment env;
+
+    @Autowired
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return String.format("Hi, there. This is a message from Second Service on PORT %s"
+                , env.getProperty("local.server.port"));
+    }
+}
