0b5dfa341c99142e928373b86c852b22b8e17923
paki1019 <paki1019@gmail.com>
2022-02-09 00:20:21 +0900

    springcloud

--- user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
+++ user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
@@ -7,7 +7,7 @@ import java.util.Date;
 @Data
 public class ResponseOrder {
     private String productId;
-    private Integer gty;
+    private Integer qty;
     private Integer unitPrice;
     private Integer totalPrice;
     private Date createdAt;
